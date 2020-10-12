ab77b2aca61440444213534761f5efa5b4065f18
wskvfhprrk <75412985@qq.com>
2020-10-12 09:57:05 +0800

    重新学习

--- src/main/java/com/hejz/security/UserApi.java
+++ src/main/java/com/hejz/security/UserApi.java
@@ -1,16 +1,11 @@
-package com.exampl.price;
+package com.hejz.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * @author: hejz
- * @Description:
- * @Date: 2020/4/20 10:18
- */
 @SpringBootApplication
-public class PriceApplication {
+public class UserApi {
     public static void main(String[] args) {
-        SpringApplication.run(PriceApplication.class, args);
+        SpringApplication.run(UserApi.class,args);
     }
 }
--- user_api/src/main/java/org/example/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends JpaSpecificationExecutor<User>, CrudRepository<User,Long> {
-    User findByUsername(String username);
-}
